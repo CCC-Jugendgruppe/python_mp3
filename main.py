0e3cb43d9dcb74eafa2a7b16c89dda5197c95bad
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-05-21 19:18:50 +0200

    extra function

--- main.py
+++ main.py
@@ -5,59 +5,64 @@ from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
 from database import Database
 import re
 
-config = configparser.ConfigParser() 
-try:
-	config.read('config.ini')
-	dirs = config.items("DIRS")
-except:
-	sys.exit("no config provided")
+def songsupdate():
+    config = configparser.ConfigParser() 
+    try:
+        config.read('config.ini')
+        dirs = config.items("DIRS")
+    except:
+        sys.exit("no config provided")
 
 
 
 
-connection = None
-db = Database(config["DATABASE"]["dir"])
-connection = db.create_connection()
-db.init_database(connection)
-print(str(connection) + "\n")
+    connection = None
+    db = Database(config["DATABASE"]["dir"])
+    connection = db.create_connection()
+    db.init_database(connection)
+    print(str(connection) + "\n")
 
-"""
-Possible Tags:
+    """
+    Possible Tags:
 
-    - artist;
-    - album;
-    - song;
-    - track;
-    - comment;
-    - year;
-    - genre;
-    - band (version 2.x);
-    - composer (version 2.x);
-    - copyright (version 2.x);
-    - url (version 2.x);
-    - publisher (version 2.x).
-"""
+        - artist;
+        - album;
+        - song;
+        - track;
+        - comment;
+        - year;
+        - genre;
+        - band (version 2.x);
+        - composer (version 2.x);
+        - copyright (version 2.x);
+        - url (version 2.x);
+        - publisher (version 2.x).
+    """
 
-for key, path in dirs: 
-    for subdir, dirs, files in os.walk(path):
-        for file in files:
-            if re.search("\.mp3$", file):
-                tags = MP3File(os.path.join(subdir, file)).get_tags()
-                if int(config["MP3"]["version"]) == 2: 
-                    print("Selected ID3TagV2")
-                    if tags["ID3TagV2"] == {}:
-                        print("no Metadata\n")
-                    else:
-                        print(tags["ID3TagV2"])
-                        print("\n")                
-                        db.update_database(connection, tags["ID3TagV2"])
-                else: 
-                    print("Selected ID3TagV1")
-                    if tags["ID3TagV1"] == {}:
-                        print("no Metadata\n")
-                    else:
-                        print(tags["ID3TagV1"])
-                        print("\n")
-                        db.update_database(connection, tags["ID3TagV1"])
+    for key, path in dirs: 
+        for subdir, dirs, files in os.walk(path):
+            for file in files:
+                if re.search("\.mp3$", file):
+                    tags = MP3File(os.path.join(subdir, file)).get_tags()
+                    if int(config["MP3"]["version"]) == 2: 
+                        print("Selected ID3TagV2")
+                        if tags["ID3TagV2"] == {}:
+                            print("no Metadata\n")
+                        else:
+                            print(tags["ID3TagV2"])
+                            print("\n")                
+                            db.update_database(connection, tags["ID3TagV2"])
+                    else: 
+                        print("Selected ID3TagV1")
+                        if tags["ID3TagV1"] == {}:
+                            print("no Metadata\n")
+                        else:
+                            print(tags["ID3TagV1"])
+                            print("\n")
+                            db.update_database(connection, tags["ID3TagV1"])
 
-db.close_connection(connection)
+    db.close_connection(connection)
+
+
+if __name__ == "__main__":
+    songsupdate()
