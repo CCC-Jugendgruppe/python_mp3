dceb0f868a3f89c59d6dc72945f59018534e712a
LeSnake04 <dev.lesnake@posteo.de>
2021-05-28 16:28:48 +0200

    delete unessisarry tabs

--- main.py
+++ main.py
@@ -28,44 +28,44 @@ def songsupdate():
 		"""
 		Possible Tags:
 
-				- artist;
-				- album;
-				- song;
-				- track;
-				- comment;
-				- year;
-				- genre;
-				- band (version 2.x);
-				- composer (version 2.x);
-				- copyright (version 2.x);
-				- url (version 2.x);
-				- publisher (version 2.x).
+			- artist;
+			- album;
+			- song;
+			- track;
+			- comment;
+			- year;
+			- genre;
+			- band (version 2.x);
+			- composer (version 2.x);
+			- copyright (version 2.x);
+			- url (version 2.x);
+			- publisher (version 2.x).
 		"""
 
 		for key, path in dirs: 
-				for subdir, dirs, files in os.walk(path):
-						for file in files:
-								if re.search("\.mp3$", file):
-										tags = MP3File(os.path.join(subdir, file)).get_tags()
-										if int(config["MP3"]["version"]) == 2: 
-												print("Selected ID3TagV2")
-												if tags["ID3TagV2"] == {}:
-														print("no Metadata\n")
-												else:
-														print(tags["ID3TagV2"])
-														print("\n")                
-														db.update_database(connection, tags["ID3TagV2"])
-										else: 
-												print("Selected ID3TagV1")
-												if tags["ID3TagV1"] == {}:
-														print("no Metadata\n")
-												else:
-														print(tags["ID3TagV1"])
-														print("\n")
-														db.update_database(connection, tags["ID3TagV1"])
+			for subdir, dirs, files in os.walk(path):
+				for file in files:
+					if re.search("\.mp3$", file):
+							tags = MP3File(os.path.join(subdir, file)).get_tags()
+							if int(config["MP3"]["version"]) == 2: 
+								print("Selected ID3TagV2")
+								if tags["ID3TagV2"] == {}:
+									print("no Metadata\n")
+								else:
+									print(tags["ID3TagV2"])
+									print("\n")                
+									db.update_database(connection, tags["ID3TagV2"])
+							else: 
+								print("Selected ID3TagV1")
+								if tags["ID3TagV1"] == {}:
+									print("no Metadata\n")
+								else:
+									print(tags["ID3TagV1"])
+									print("\n")
+									db.update_database(connection, tags["ID3TagV1"])
 
 		db.close_connection(connection)
 
 
 if __name__ == "__main__":
-				songsupdate()
+	songsupdate()
