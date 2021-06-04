4c98b6cd589f76e26e0a88ca6452ef7a35547ee9
LeSnake04 <dev.lesnake@posteo.de>
2021-06-04 19:14:32 +0200

    updated window

--- window.py
+++ window.py
@@ -25,11 +25,31 @@ class Window:
 		ttk.Button(mainframe,text="refresh database", command=songsupdate).grid()
 		#title.option_add('*font','bold 20') TODO: Big font size for Title
 		tk.Text(mainframe, width=40, height=10)
+		outputtabledict={"songname" : ["test", None, None, "test", "test","test"]}
+		outputtablevalues=tk.StringVar(value=outputtabledict)
+		outputtable=tk.Listbox(mainframe,listvariable=outputtablevalues).grid()
+		print(outputtabledict)
+		for i, v in outputtabledict.items():
+			print(i)
+			print(v)
+			outputtabledict[i] = list(filter(lambda a: a !=None, outputtabledict[i]))
+				#for j in v:
+				#	"""
+				#	if j == None:
+						# remove None entries from the Array
+				#		outputtabledict[i].remove(j)
+				#		j = outputtabledict[i][j] 
+				#	"""
+		print(outputtabledict)
 		
+		
+	#def parseoutputtable():
+	#	for i in outputtabledict:
+	#		if i == 
+	
+	def refreshoutputtable():
+		outputtablevalues.set(outputtabledict)
 
 	def run(self):
 		self.root.mainloop()
 
-windowinstance = Window()
-windowinstance.run()
-#songsupdate()
