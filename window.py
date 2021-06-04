63f732925647a3eb862324dedc145e607bc06352
LeSnake04 <dev.lesnake@posteo.de>
2021-06-04 17:58:46 +0200

    update

--- window.py
+++ window.py
@@ -3,28 +3,33 @@
 # https://tkdocs.com/tutorial/index.html
 # https://www.tutorialspoint.com/python/python_gui_programming.htm
 
-from main import songsupdate
+from core import songsupdate
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.font as tkf
 
-# setup
-root = tk.Tk()
-root.title("Songs")
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+class Window: 
+	# setup
+	def __init__(self):
+		self.root = tk.Tk()
+		self.root.title("Songs")
+		mainframe = ttk.Frame(self.root, padding="3 3 12 12")
+		mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+		self.root.columnconfigure(0, weight=1)
+		self.root.rowconfigure(0, weight=1)
 
-# global font
-root.option_add('*font','10')
+		# global font
+		self.root.option_add('*font','10')
+		# main window
+		tk.Label(mainframe, text="Songs").grid()
+		ttk.Button(mainframe,text="refresh database", command=songsupdate).grid()
+		#title.option_add('*font','bold 20') TODO: Big font size for Title
+		tk.Text(mainframe, width=40, height=10)
+		
 
-# main window
-title=ttk.Label(mainframe, text="Songs").grid()
-#title.option_add('*font','bold 20') TODO: Big font size for Title
-searchbar = tk.Text(mainframe, width=40, height=10)
+	def run(self):
+		self.root.mainloop()
 
-
-root.mainloop()
-
+windowinstance = Window()
+windowinstance.run()
 #songsupdate()
