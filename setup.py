d47e192b3893e6bf93a32c09b4f3411353dbd6c1
LeSnake04 <dev.lesnake@posteo.de>
2021-06-17 08:59:28 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
 	name='python_mp3',
 	version='0.0.1',
-	packages=find_packages('pyqt6','mp3_tagger')
+	install_requires=[
+		'pyqt6',
+		'mp3_tagger'
+	]
 )
