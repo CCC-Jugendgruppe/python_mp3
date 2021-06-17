0a397dbf1761e555eb23046f75b0e877d742ad99
LeSnake04 <dev.lesnake@posteo.de>
2021-06-17 15:13:10 +0200

    setup installs package as global command

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup,find_packages
 
 setup(
-	name='python_mp3',
+	name='python-mp3',
 	version='0.0.1',
+	author='CCC Jugendgruppe Göttingen',
+	url='https://github.com/CCC-Jugendgruppe/python_mp3',
+	#py_modules=[],
+	packages=find_packages(include=['python_mp3','python_mp3.*']),
 	install_requires=[
 		'pyqt6',
 		'mp3_tagger'
-	]
+	],
+	entry_points={
+		'console_scripts': ['python-mp3=python_mp3.main:main']
+	}
 )
