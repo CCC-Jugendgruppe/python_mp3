767781be324b244e52148453d15cfb0666aa7c4f
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-22 20:26:18 +0200

    flowed the python style guidelines

--- python_mp3/window.py
+++ python_mp3/window.py
@@ -13,172 +13,177 @@ import PyQt6.QtWidgets as qtw
 import PyQt6.QtGui as qtg
 
 # Set path for temporary database to cache 
-tmp_db_path=str(pathlib.Path.home())+'/.cache/python_mp3_tmp.sql'
+tmp_db_path = str(pathlib.Path.home()) + '/.cache/python_mp3_tmp.sql'
 # 
-tmp_input_path=['./input/']
-config_path='./python_mp3.conf'
+tmp_input_path = ['./input/']
+config_path = './python_mp3.conf'
+
 
 class Window(qtw.QWidget):
-	def __init__(self):
-		super().__init__()
-		self.initUI()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-	settings={}
-	def initUI(self):
-		# Global Settings
-		self.setGeometry(0, 0, 650, 500)
-		self.setWindowTitle('Python mp3')
-		self.setFont(qtg.QFont('SansSerif', 10))
-		
-		self.resetSettings
-		#self.loadSettings()
-		print(self.settings)
+    settings = {}
 
-		# General Layout
-		mainlayout = qtw.QVBoxLayout()
-		# Splitter between Output and Settings
-		mainsplitter = qtw.QSplitter(qtc.Qt.Orientation.Horizontal)
-		mainsplitter.addWidget(self.outputFrame())
-		mainsplitter.addWidget(self.settingsFrame())
-		mainlayout.addWidget(mainsplitter)
-		# Buttom
-		mainlayout.addLayout(self.bottomPanel())
+    def initUI(self):
+        # Global Settings
+        self.setGeometry(0, 0, 650, 500)
+        self.setWindowTitle('Python mp3')
+        self.setFont(qtg.QFont('SansSerif', 10))
 
-		self.setLayout(mainlayout)
-		self.show()
-	
-	def outputFrame(self):
-		# General Layout Settings
-		layout=self.setupLayout('v')
+        self.resetSettings
+        # self.loadSettings()
+        print(self.settings)
 
-		# Songs Label
-		songslabel = self.titleLabel('Songs')
-		layout.addWidget(songslabel)
+        # General Layout
+        mainlayout = qtw.QVBoxLayout()
+        # Splitter between Output and Settings
+        mainsplitter = qtw.QSplitter(qtc.Qt.Orientation.Horizontal)
+        mainsplitter.addWidget(self.outputFrame())
+        mainsplitter.addWidget(self.settingsFrame())
+        mainlayout.addWidget(mainsplitter)
+        # Buttom
+        mainlayout.addLayout(self.bottomPanel())
 
-		# Button to Refresh Database
-		refreshbtn = qtw.QPushButton('Refresh', self)
-		refreshbtn.setToolTip('Refresh the Database')
-		refreshbtn.clicked.connect(self.refreshTmpDb)
-		#TODO: Read config from settings panel
-		refreshbtn.resize(refreshbtn.sizeHint())
-		layout.addWidget(refreshbtn)
+        self.setLayout(mainlayout)
+        self.show()
 
-		#songstable=self.createSongsTable()
-		#songstable.show()
-		#leftlayout.addWidget(songstable)
+    def outputFrame(self):
+        # General Layout Settings
+        layout = self.setupLayout('v')
 
-		frame = self.setupFrame(layout)
-		return frame
+        # Songs Label
+        songslabel = self.titleLabel('Songs')
+        layout.addWidget(songslabel)
 
-	def settingsFrame(self):
-		layout = self.setupLayout('v')
+        # Button to Refresh Database
+        refreshbtn = qtw.QPushButton('Refresh', self)
+        refreshbtn.setToolTip('Refresh the Database')
+        refreshbtn.clicked.connect(self.refreshTmpDb)
+        # TODO: Read config from settings panel
+        refreshbtn.resize(refreshbtn.sizeHint())
+        layout.addWidget(refreshbtn)
 
-		settingslabel = self.titleLabel('Settings')
-		layout.addWidget(settingslabel)
+        self.createSongsTable()
+        # songstable = self.createSongsTable()
+        # songstable.show()
+        # leftlayout.addWidget(songstable)
 
-		frame = self.setupFrame(layout)
-		return frame
+        frame = self.setupFrame(layout)
+        return frame
 
-	def bottomPanel(self):
-		layout = self.setupLayout('h')
-		
-		# Button to reset settings
-		resetbtn = qtw.QPushButton('Reset')
-		resetbtn.clicked.connect(self.resetSettings)
-		resetbtn.resize(resetbtn.sizeHint())
-		resetbtn.setToolTip('Reset configuration')
-		layout.addWidget(resetbtn)
+    def settingsFrame(self):
+        layout = self.setupLayout('v')
 
-		# Button to export Database
-		exportbtn = qtw.QPushButton('Export')
-		layout.addWidget(exportbtn)
+        settingslabel = self.titleLabel('Settings')
+        layout.addWidget(settingslabel)
 
-		# Button to Quit Programm
-		quitbtn = qtw.QPushButton('Quit')
-		quitbtn.clicked.connect(self.quit)
-		quitbtn.resize(quitbtn.sizeHint())
-		quitbtn.setToolTip('Exit the Programm')
-		layout.addWidget(quitbtn)
+        frame = self.setupFrame(layout)
+        return frame
 
-		return layout
+    def bottomPanel(self):
+        layout = self.setupLayout('h')
 
-	def createSongsTable(self):
-		#TODO List with Songs
-		self.refreshTmpDb()
-		songsdb = Database(tmp_db_path)
-		songsdict = songsdb
-		print(songsdict)
-		#print(len(songsdict))
+        # Button to reset settings
+        resetbtn = qtw.QPushButton('Reset')
+        resetbtn.clicked.connect(self.resetSettings)
+        resetbtn.resize(resetbtn.sizeHint())
+        resetbtn.setToolTip('Reset configuration')
+        layout.addWidget(resetbtn)
 
-		#songstable = qtw.QTableWidget()
-		#songstable.setRowCount(len(songsdict))
-		#horHeaders = []
-		#for n, key in enumerate(sorted(songsdict)):
-		#	horHeaders.append(key)
-		#	for m, item in enumerate(songsdict):
-		#		newitem = qtw.QTableWidgetItem(item)
-		#		self.setItem(m, n, newitem)
-		#self.setHorizontalHeaderLabels(horHeaders)
+        # Button to export Database
+        exportbtn = qtw.QPushButton('Export')
+        layout.addWidget(exportbtn)
 
-		#return songstable
-	
-	def setupLayout(self,orientation):
-		if orientation  is 'v':
-			layout = qtw.QVBoxLayout()
-		elif orientation is 'h':
-			layout = qtw.QHBoxLayout()
-		else:
-			sys.exit('setupLayout: orientation must be \'v\' or \'h\'')
-		layout.setContentsMargins(0,0,0,0)
-		layout.setSpacing(0)
-		layout.setEnabled(True)
-		return layout
+        # Button to Quit Programm
+        quitbtn = qtw.QPushButton('Quit')
+        quitbtn.clicked.connect(self.quit)
+        quitbtn.resize(quitbtn.sizeHint())
+        quitbtn.setToolTip('Exit the Programm')
+        layout.addWidget(quitbtn)
 
-	def setupFrame(self,layout):
-		frame = qtw.QFrame()
-		frame.setLayout(layout)
-		frame.setFrameShape(qtw.QFrame.Shape.StyledPanel)
-		return frame
+        return layout
 
-	def titleLabel(self,name):
-		label = qtw.QLabel('<b>'+name+'<\b>', self)	
-		label.setFont(qtg.QFont('Ubuntu', 15))
-		return label
+    def createSongsTable(self):
+        # TODO List with Songs
+        self.refreshTmpDb()
+        songsdb = Database(tmp_db_path)
+        songsdict = songsdb
+        print(songsdict)
 
-	def refreshTmpDb(self):	
-		#print('Input:',tmp_input_path, 'Output:', tmp_db_path)
-		songsupdate(tmp_input_path,tmp_db_path,2)
+    # print(len(songsdict))
 
-	def quit(self):
-		self.saveSettings()
-		qtw.QApplication.instance().quit()
+    # songstable = qtw.QTableWidget()
+    # songstable.setRowCount(len(songsdict))
+    # horHeaders = []
+    # for n, key in enumerate(sorted(songsdict)):
+    #	horHeaders.append(key)
+    #	for m, item in enumerate(songsdict):
+    #		newitem = qtw.QTableWidgetItem(item)
+    #		self.setItem(m, n, newitem)
+    # self.setHorizontalHeaderLabels(horHeaders)
 
-	def resetSettings(self):
-		print('Resetting Configuration')
-		self.settings={
-			'paths':[],
-			'mp3_version':2
-		}
-		print(self.settings)
+    # return songstable
 
-	def saveSettings(self):
-		print('Saving settings... '+ str(self.settings))
-		with open(config_path, 'w', encoding='utf8') as f:
-			f.write(json.dumps(self.settings,indent=2,ensure_ascii=False))
-	
-	def loadSettings(self):
-		print('Loading settings...')
-		if os.path.isfile(config_path):
-			with open(config_path, 'r', encoding='utf8') as f:
-				self.settings=json.load(f)
-		else:
-			self.resetSettings()
-		print('Loaded settings... '+str(self.settings))
+    def setupLayout(self, orientation):
+        if orientation is 'v':
+            layout = qtw.QVBoxLayout()
+        elif orientation is 'h':
+            layout = qtw.QHBoxLayout()
+        else:
+            sys.exit('setupLayout: orientation must be \'v\' or \'h\'')
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        layout.setEnabled(True)
+        return layout
+
+    def setupFrame(self, layout):
+        frame = qtw.QFrame()
+        frame.setLayout(layout)
+        frame.setFrameShape(qtw.QFrame.Shape.StyledPanel)
+        return frame
+
+    def titleLabel(self, name):
+        label = qtw.QLabel('<b>' + name + '<\b>', self)
+        label.setFont(qtg.QFont('Ubuntu', 15))
+        return label
+
+    def refreshTmpDb(self):
+        # print('Input:',tmp_input_path, 'Output:', tmp_db_path)
+        songsupdate(tmp_input_path, tmp_db_path, 2)
+
+    def quit(self):
+        self.saveSettings()
+        qtw.QApplication.instance().quit()
+
+    def resetSettings(self):
+        print('Resetting Configuration')
+        self.settings = {
+            'paths': [],
+            'mp3_version': 2
+        }
+        print(self.settings)
+
+    def saveSettings(self):
+        print('Saving settings... ' + str(self.settings))
+        with open(config_path, 'w', encoding='utf8') as f:
+            f.write(json.dumps(self.settings, indent=2, ensure_ascii=False))
+
+    def loadSettings(self):
+        print('Loading settings...')
+        if os.path.isfile(config_path):
+            with open(config_path, 'r', encoding='utf8') as f:
+                self.settings = json.load(f)
+        else:
+            self.resetSettings()
+        print('Loaded settings... ' + str(self.settings))
+
 
 def createWindow():
-	app = qtw.QApplication(sys.argv)
-	app.setStyle('Fusion')
-	#app.setPalette(colorpalette())
-	win=Window()
-	#win.saveSettings()
-	sys.exit(app.exec())
+    app = qtw.QApplication(sys.argv)
+    app.setStyle('Fusion')
+    # app.setPalette(colorpalette())
+    win = Window()
+    # win.saveSettings()
+    sys.exit(app.exec())
