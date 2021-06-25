4c74eb0a5b513882fca72246ae44de88974406cb
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-25 18:32:06 +0200

    config file

--- python_mp3/config.py
+++ python_mp3/config.py
@@ -1,4 +1,18 @@
 
-class Config():
-    def __init__(self):
-        print("foo")
+class Config:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def readfile(self):
+        with open('dog_breeds.txt', 'r') as reader:
+            print(reader.read())
+        print("createnew")
+
+    def createnew(self):
+        print("createnew")
+
+    def update(self):
+        print("update")
+
+    def clear(self):
+        print("clear")
