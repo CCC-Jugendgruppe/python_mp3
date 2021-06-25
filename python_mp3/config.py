d59a6ccbb97346139c7943a7406c495a58c19576
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-25 18:42:11 +0200

    config

--- python_mp3/config.py
+++ python_mp3/config.py
@@ -1,12 +1,17 @@
+import os
 
 class Config:
     def __init__(self, filename):
         self.filename = filename
 
     def readfile(self):
-        with open('dog_breeds.txt', 'r') as reader:
-            print(reader.read())
-        print("createnew")
+        print(str(self.filename))
+        try:
+            with open(str(self.filename), 'r') as reader:
+                print(reader.read())
+        except:
+            print("diese Datei gibt es nicht")
+            self.createnew()
 
     def createnew(self):
         print("createnew")
@@ -16,3 +21,7 @@ class Config:
 
     def clear(self):
         print("clear")
+
+
+test = Config("/config.json")
+test.readfile()
