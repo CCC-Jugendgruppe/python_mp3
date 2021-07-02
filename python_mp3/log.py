2dd6503d045eb095a11398645f912572a19f8e69
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-07-02 19:24:27 +0200

    logfile

---
+++ python_mp3/log.py
@@ -0,0 +1,7 @@
+class Log:
+	
+	def info(self, meassage): print("[Info]: " + meassage)
+	
+	def warning(self, warning): print("[Warning]: " + warning)
+	
+	def error(self, error): print("[Error]: " + error)
